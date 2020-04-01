7df720be500c661c6e33560bb6c3c8060866733d
burnout87 <gabriele.barni@gmail.com>
2020-04-01 23:14:26 +0200

    second commit

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,28 +1,23 @@
-import { Component, OnInit, SimpleChanges, Input } from '@angular/core';
+import { Component, OnInit, SimpleChanges } from '@angular/core';
 import { Subject } from 'rxjs';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
+
 export class MapComponent implements OnInit {
 
   private map;
   private onChanges = new Subject<SimpleChanges>();
-  // There are two ways of detecting and acting up on when an input changes in the child component
-  @Input() activityToPlot:Activity;
 
   constructor() { }
 
   ngOnInit() {
-    this.onChanges.subscribe((data:SimpleChanges)=>{
-      this.cleanMap();
-      // this.plotMarker(data.activityToPlot.currentValue.start_latlng);
-      // this.plotPolyline(data.activityToPlot.currentValue.map.summary_polyline);
-    });
+    this.initMap();
   }
-
 
   public cleanMap() {
     for(var i in this.map._layers) {
@@ -65,44 +60,6 @@ export class MapComponent implements OnInit {
     // var height = this.map.getBounds().getNorth() - this.map.getBounds().getSouth();
 
     return this.map.getBounds();
-  }
-
-  public checkPlotVisible(plot: any) {
-    var pol:any = L.polyline(plot);
-    return this.map.getBounds().intersects( pol.getBounds() );
-  }
-
-  public plotActivity(activity: Activity) {
-    var pol:any = L.polyline(
-      activity.decodedMap,
-      {
-          color: 'blue',
-          weight: 2,
-          opacity: .7,
-          lineJoin: 'round'
-      });
-    pol.on('mouseover', function() {
-      this.setStyle({
-        weight: 6
-      });
-    });
-    pol.on('mouseout', function() {
-      this.setStyle({
-        weight: 2
-      });
-    });
-    pol.bindPopup("<b><a href=\"https://www.strava.com/activities/" + activity.id + "\" target=\"_blank\">" + activity.name + "</a></b><br>" + moment(activity.start_date_local).format("dddd, MMMM Do YYYY, h:mm:ss a"));
-    pol.addTo(this.map);
-    var marker = L.marker(activity.start_latlng);
-    marker.on('mouseover', function() {
-      pol.setStyle({ weight: 6});
-    });
-    marker.on('mouseout', function() {
-      pol.setStyle({ weight: 2});
-    });
-    //marker.options = activity;
-    marker.addTo(this.map);
-    marker.bindPopup("<b><a href=\"https://www.strava.com/activities/" + activity.id + "\" target=\"_blank\">" + activity.name + "</a></b><br>" + moment(activity.start_date_local).format("dddd, MMMM Do YYYY, h:mm:ss a"));
   }
 
 }

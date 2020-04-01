7a324ad48d7f779ae0a78b9ed5b01a822796d462
Alfredo <alfredo.lanzetta.al@gmail.com>
2020-04-01 23:41:14 +0200

    server setup

---
+++ server/index.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use('/', express.static('dist/up-frontend'))
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))

67ce455266d9e03fdf0154c83f8b678fe2e1e0f4
Alfredo <alfredo.lanzetta.al@gmail.com>
2020-04-04 16:39:53 +0200

    some css rules

--- src/app/mocked-negozianti.ts
+++ src/app/mocked-negozianti.ts
@@ -2,18 +2,18 @@ import { Negoziante } from './negoziante';
 
 export const NEGOZIANTI: Negoziante[] = [
     {
-        id: "ac-1",
-        name: "Central Air Conditioner",
-        description: "Keep the whole office cool with this Central Air Conditioner. Removes up to 48,000 BTUs of heat per hour."
+        id: "1",
+        name: "Pippo Pizza Pazza",
+        description: "pizzeria."
     },
     {
-        id: "ac-2",
-        name: "Window Air Conditioner",
-        description: "Perfect to keep a room or small apartment cool."
+        id: "2",
+        name: "Panificio all'angolo",
+        description: "food store."
     },
     {
-        id: "ac-3",
-        name: "A fan",
-        description: "An inexpensive, but effective way to stop your coworkers from complaining about the heat."
+        id: "3",
+        name: "Pettina i coccodrilli",
+        description: "animal store."
     },
-] 
+]

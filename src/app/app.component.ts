b26650ca05f1941e15b35188df5c00a3bb31e3b1
Alfredo <alfredo.lanzetta.al@gmail.com>
2020-04-04 04:42:45 +0200

    update map and add markers on it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,18 +9,40 @@ import { isPlatformBrowser } from '@angular/common';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'upFrontend';
   router: Router;
   isBrowser: boolean;
-  latitude = -28.68352;
-  longitude = -147.20785;
-  mapType = 'terrain';
+  latitude = 41.6650266;
+  longitude = 12.8701779;
+  mapType = 'roadmap'; //types https://mkyong.com/google-maps/google-maps-api-map-types-example/
   zoom = 5;
+
+  ngOnInit() {
+          this.location = {
+            markers : [{
+                          lat: 41.6650266,
+                          lng: 12.8701779
+                      }]
+          }
+        }
 
   constructor(private _router: Router, @Inject(PLATFORM_ID) platformId: Object){
     this.isBrowser = isPlatformBrowser(platformId);
-    this.router = _router; 
+    this.router = _router;
   }
 
+}
+
+interface Marker {
+    lat: number;
+    lng: number;
+}
+
+interface Location {
+    latitude: number;
+    longitude: number;
+    mapType: ?string;
+    zoom: ?number;
+    marker: Marker;
 }

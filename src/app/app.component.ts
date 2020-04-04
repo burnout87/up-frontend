17676a062abf97362ec559ff6d5416a522b1ac31
Alfredo <alfredo.lanzetta.al@gmail.com>
2020-04-04 04:45:57 +0200

    add comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,15 +15,22 @@ export class AppComponent implements OnInit{
   isBrowser: boolean;
   latitude = 41.6650266;
   longitude = 12.8701779;
-  mapType = 'roadmap'; //types https://mkyong.com/google-maps/google-maps-api-map-types-example/
+  mapType = 'roadmap';
   zoom = 5;
 
+  // init map
+  // TODO load data from API
+  // check -> this.http.get<Address>("url")
   ngOnInit() {
           this.location = {
             markers : [{
                           lat: 41.6650266,
                           lng: 12.8701779
-                      }]
+                      },
+                      {
+                                    lat: 45.6650266,
+                                    lng: 12.8701779
+                                }]
           }
         }
 

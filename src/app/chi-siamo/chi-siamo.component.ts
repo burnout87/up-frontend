4b70ef595fc44a625f19cf3dc555532bf3709b4c
El Drago <eldrago97@gmail.com>
2020-04-18 19:42:58 +0200

    Fix vari

--- src/app/chi-siamo/chi-siamo.component.ts
+++ src/app/chi-siamo/chi-siamo.component.ts
@@ -1,16 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-// import { MatSliderModule } from '@angular/material/slider';
-// import { MatCardModule } from '@angular/material/card';
-
-
-// @MatSliderModule({
-=======
 import { MatSliderModule } from '@angular/material/slider';
 import { MEMBRI } from '../mocked-members';
 import { Member } from '../member';
 // @MatSliderModule ({
->>>>>>> a0f64b1e1fa91e7cf012005c1bda576c25d87606
 //   selector: 'slider-overview-example',
 //   templateUrl: 'slider-overview-example.html',
 //   styleUrls: ['slider-overview-example.css'],

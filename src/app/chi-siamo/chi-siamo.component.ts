6e50504540474c7a8007af59927b8ee12a70fc3f
Mattia Robino <mattia.robino@consoft.it>
2020-04-29 21:03:54 +0200

    fix chi siamo

--- src/app/chi-siamo/chi-siamo.component.ts
+++ src/app/chi-siamo/chi-siamo.component.ts
@@ -3,6 +3,7 @@ import { MatSliderModule } from '@angular/material/slider';
 import { MEMBRI } from '../mocked-members';
 import { Member } from '../member';
 import Typed from 'typed.js';
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 // @MatSliderModule ({
 //   selector: 'slider-overview-example',
 //   templateUrl: 'slider-overview-example.html',
@@ -25,16 +26,30 @@ export class ChiSiamoComponent implements OnInit {
 
   membri:Member[] = MEMBRI;
 
-  constructor() { }
+  public isMax;
+
+  constructor( private breakpointObserver: BreakpointObserver ) {
+
+    this.breakpointObserver
+    .observe(['(max-width: 990px)'])
+    .subscribe((state: BreakpointState) => {
+      if (state.matches) {
+        this.isMax = true;
+      } else {
+        this.isMax = false;
+      }
+    });
+
+   }
 
   ngOnInit() {
     var typed = new Typed('#typed', {
       strings: [
-        '<span class="mat-display-3">Siamo ^1000 <span style="color:#593C8F"> imprenditori</span></span>', 
-        '<span class="mat-display-3">Siamo ^1000 <span style="color:#593C8F"> volontari</span></span>', 
-        '<span class="mat-display-3">Siamo ^1000 <span style="color:#593C8F"> marketers</span></span>', 
-        '<span class="mat-display-3">Siamo ^1000 <span style="color:#593C8F"> persone</span></span>',
-        '<span class="mat-display-3">Siamo ^1000 <span style="color:#593C8F"> designers</span></span>'
+        'Siamo ^1000 <span style="color:#593C8F"> imprenditori</span>', 
+        'Siamo ^1000 <span style="color:#593C8F"> volontari</span>', 
+        'Siamo ^1000 <span style="color:#593C8F"> marketers</span>', 
+        'Siamo ^1000 <span style="color:#593C8F"> persone</span>',
+        'Siamo ^1000 <span style="color:#593C8F"> designers</span>'
       ],
       loop: true,
       typeSpeed: 80

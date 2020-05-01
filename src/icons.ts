adbf1f0a3e7824bc6d03169057f456e0880540a0
Mattia Robino <mattia.robino@consoft.it>
2020-05-01 20:31:20 +0200

    added partners component and share buttons to homepage

--- src/icons.ts
+++ src/icons.ts
@@ -4,10 +4,11 @@ import { faFacebookF } from '@fortawesome/free-brands-svg-icons/faFacebookF';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
 import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons/faEnvelope';
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons/faWhatsapp';
 
 
 const icons = [
-    faFacebookF, faTwitter, faLinkedinIn, faEnvelope
+    faFacebookF, faTwitter, faLinkedinIn, faEnvelope, faWhatsapp
   ];
 
 library.add(...icons);

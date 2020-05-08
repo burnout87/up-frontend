061a95b0454f096e218541c4d057c9890cef139e
Mattia Robino <mattia.robino@consoft.it>
2020-05-08 20:06:49 +0200

    fix typography, headers, responsiveness

--- src/app/domande/domande.component.ts
+++ src/app/domande/domande.component.ts
@@ -1,13 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, BreakpointState, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-domande',
   templateUrl: './domande.component.html',
-  styleUrls: ['./domande.component.css']
+  styleUrls: ['./domande.component.scss']
 })
 export class DomandeComponent implements OnInit {
 
-  constructor() { }
+  public isS;
+  public isM;
+  public isL;
+
+  constructor( private breakpointObserver: BreakpointObserver ) { 
+
+    this.breakpointObserver
+    .observe([Breakpoints.XSmall, Breakpoints.HandsetPortrait])
+    .subscribe((state: BreakpointState) => {
+      if (state.matches) {
+        this.isS = true;
+      } else {
+        this.isS = false;
+      }
+    });
+
+    this.breakpointObserver
+    .observe([Breakpoints.Small, Breakpoints.Medium])
+    .subscribe((state: BreakpointState) => {
+      if (state.matches) {
+        this.isM = true;
+      } else {
+        this.isM = false;
+      }
+    });
+
+    this.breakpointObserver
+    .observe([Breakpoints.Large, Breakpoints.XLarge])
+    .subscribe((state: BreakpointState) => {
+      if (state.matches) {
+        this.isL = true;
+      } else {
+        this.isL = false;
+      }
+    });
+
+  }
 
   ngOnInit() {
   }

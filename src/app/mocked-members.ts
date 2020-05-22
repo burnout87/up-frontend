6aa1418fd18b5aa721e9756df69e19724ec2ebfd
burnout87 <gabriele.barni@gmail.com>
2020-05-22 17:37:55 +0200

    more images and fied images for chi-siamo

--- src/app/mocked-members.ts
+++ src/app/mocked-members.ts
@@ -5,121 +5,121 @@ export const MEMBRI: Member[] = [
         id: "1",
         name: "Elena",
         role: "Innovative Business Developer",
-        imgUrl: "/assests/imgs/faces/Elena-B.jpg"
+        imgUrl: "/assets/imgs/faces/Elena-B.jpg"
     },
     {
         id: "2",
         name: "Alfredo",
         role: "Full Stack Developer",
-        imgUrl: "/assests/imgs/faces/Alfredo.jpg"
+        imgUrl: "/assets/imgs/faces/Alfredo.jpg"
     },
     {
         id: "3",
         name: "Giona",
         role: "Project Manager & Retail Expert",
-        imgUrl: "/assests/imgs/faces/Giona.jpg"
+        imgUrl: "/assets/imgs/faces/Giona.jpg"
     },
     {
         id: "4",
         name: "Daniele",
         role: "Imprenditore",
-        imgUrl: "/assests/imgs/faces/Daniele.jpg"
+        imgUrl: "/assets/imgs/faces/Daniele.jpg"
     },
     {
         id: "5",
         name: "Lara",
         role: "UX Designer & Researcher",
-        imgUrl: "/assests/imgs/faces/Lara.jpg"
+        imgUrl: "/assets/imgs/faces/Lara.jpg"
     },
     {
         id: "6",
         name: "Davide",
         role: "Product Designer",
-        imgUrl: "/assests/imgs/faces/Davide.jpg"
+        imgUrl: "/assets/imgs/faces/Davide.jpg"
     },
     {
         id: "7",
         name: "Nicola",
         role: "Management Engineer",
-        imgUrl: "/assests/imgs/faces/Nicola.jpg"
+        imgUrl: "/assets/imgs/faces/Nicola.jpg"
     },
     {
         id: "8",
         name: "Chiara",
         role: "Brand Communication Specialist",
-        imgUrl: "/assests/imgs/faces/Chiara.jpg"
+        imgUrl: "/assets/imgs/faces/Chiara.jpg"
     },
     {
         id: "9",
         name: "Lorenzo",
         role: "Energy Engineer",
-        imgUrl: "/assests/imgs/faces/Lorenzo.jpg"
+        imgUrl: "/assets/imgs/faces/Lorenzo.jpg"
     },
     {
         id: "10",
         name: "Elena",
         role: "Product Operations Manager",
-        imgUrl: "/assests/imgs/faces/Elena-C.jpg"
+        imgUrl: "/assets/imgs/faces/Elena-C.jpg"
     },
     {
         id: "11",
         name: "Alessio",
         role: "Front End Developer",
-        imgUrl: "/assests/imgs/faces/Alessio.jpg"
+        imgUrl: "/assets/imgs/faces/Alessio.jpg"
     },
     {
         id: "12",
         name: "Federico",
         role: "Social Innovation Specialist",
-        imgUrl: "/assests/imgs/faces/Federico.jpg"
+        imgUrl: "/assets/imgs/faces/Federico.jpg"
     },
     {
         id: "13",
         name: "Andrea",
         role: "Project Manager",
-        imgUrl: "/assests/imgs/faces/Andrea.jpg"
+        imgUrl: "/assets/imgs/faces/Andrea.jpg"
     },
     {
         id: "14",
         name: "Gianluca",
         role: "Web Marketing Strategist",
-        imgUrl: "/assests/imgs/faces/Gianluca.jpg"
+        imgUrl: "/assets/imgs/faces/Gianluca.jpg"
     },
     {
         id: "15",
         name: "Gabriele",
         role: "Full Stack Developer",
-        imgUrl: "/assests/imgs/faces/Gabriele.jpg"
+        imgUrl: "/assets/imgs/faces/Gabriele.jpg"
     },
     {
         id: "16",
         name: "Elide",
         role: "Supply Chain Specialist",
-        imgUrl: "/assests/imgs/faces/Elide.jpg"
+        imgUrl: "/assets/imgs/faces/Elide.jpg"
     },
     {
         id: "17",
         name: "Martina",
         role: "Investment & Market Strategist",
-        imgUrl: "/assests/imgs/faces/Martina.jpg"
+        imgUrl: "/assets/imgs/faces/Martina.jpg"
     },
     {
         id: "18",
         name: "Enrico",
         role: "Data Scientist",
-        imgUrl: "/assests/imgs/faces/Enrico.jpg"
+        imgUrl: "/assets/imgs/faces/Enrico.jpg"
     },
     {
         id: "19",
         name: "Irene",
         role: "Service & Graphic Designer",
-        imgUrl: "/assests/imgs/faces/Irene.jpg"
+        imgUrl: "/assets/imgs/faces/Irene.jpg"
     },
     {
         id: "20 ",
         name: "Valerio",
         role: "Marketing & Communication Specialist",
-        imgUrl: "/assests/imgs/faces/Valerio.jpg"
+        imgUrl: "/assets/imgs/faces/Valerio.jpg"
     },
     {
         id: "21 ",

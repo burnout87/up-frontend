cb31c388b9ee87a92ea097869aacb40379c762e9
burnout87 <gabriele.barni@gmail.com>
2020-05-26 00:20:53 +0200

    fixed links environemnt for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  posts: 'https://unitipossiamo.it/wp-json/wp/v2/posts',
-  media: 'https://unitipossiamo.it/wp-json/wp/v2/media',
+  posts: 'https://unitipossiamo.it:8443/wp-json/wp/v2/posts',
+  media: 'https://unitipossiamo.it:8443/wp-json/wp/v2/media',
   stores: 'https://www.unitipossiamo.it/api/negozianti',
   platforms: 'https://www.unitipossiamo.it/api/platforms',
   readyData: 'https://www.unitipossiamo.it/api/readydata',
